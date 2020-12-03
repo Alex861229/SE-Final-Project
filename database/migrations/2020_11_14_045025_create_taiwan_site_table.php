78006343d8e7cd98d6f3672d36e60adcb75dd65d
bocheng47 <bocheng4o7@gmail.com>
2020-12-03 19:24:39 +0800

    新增or修改 台韓景點Table 台韓留言Table、新增SiteController的siteAllMsg、search、index function

--- database/migrations/2020_11_14_045025_create_taiwan_site_table.php
+++ database/migrations/2020_11_14_045025_create_taiwan_site_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateKrTable extends Migration
+class CreateTaiwanSiteTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,21 +13,15 @@ class CreateKrTable extends Migration
      */
     public function up()
     {
-        Schema::create('kr_table', function (Blueprint $table) {
-            $table->increments('id');
+        Schema::create('taiwan_site', function (Blueprint $table) {
+            $table->bigIncrements('id');
             $table->string('name', 100);
             $table->string('description', 5000);
-            $table->string('address1', 1000);
-            $table->string('address2', 1000);
+            $table->string('address', 1000);
             $table->decimal('longitude', 10, 5);
             $table->decimal('latitude', 10, 5);
             $table->string('parkinginfo', 100);
-            $table->string('public_facility', 1000);
-            $table->string('accommodation', 1000);
-            $table->string('sports_facility', 1000);
-            $table->string('entertainment_facility', 1000);
-            $table->string('support_facility', 1000);
-            $table->string('name_kr', 100);
+            $table->string('ticketinfo', 100);
         });
     }
 
@@ -38,7 +32,7 @@ class CreateKrTable extends Migration
      */
     public function down()
     {
-        Schema::table('kr_table', function (Blueprint $table) {
+        Schema::table('taiwan_site', function (Blueprint $table) {
             //
         });
     }
